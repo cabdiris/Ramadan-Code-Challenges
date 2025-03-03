d2c93017bbc41fccabdc4a852a98bbcef12f4711
Cabdirisaaq <148750922+cabdiris@users.noreply.github.com>
2025-03-04 00:29:24 +0300

    Update solution-abdirisak.kt

--- week-1/solution-abdirisak.kt
+++ week-1/solution-abdirisak.kt
@@ -1,4 +1,16 @@
 
-fun main(){
-	println("ali")
+
+fun main() {
+    println("Count: ${countDuplicates("aabBcde")}")
+
+}
+fun countDuplicates(text: String) {
+    val charCounts = text.lowercase()
+        .groupingBy { it }
+        .eachCount()
+        .filter { it.value > 1 }
+
+
+
+    println("${charCounts.size}  (${charCounts.keys.joinToString(" and ")} are duplicate)")
 }
